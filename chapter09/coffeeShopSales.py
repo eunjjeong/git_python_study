dc41398e50f9d178bb39b18764fc854adc7c957e
kimeunjeong <bigi5@naver.com>
2019-11-27 12:41:22 +0900

    191127 chapter10 module 수업

--- chapter09/coffeeShopSales.py
+++ chapter09/coffeeShopSales.py
@@ -22,20 +22,11 @@ if __name__ == "__main__":
             cappucino.append(int(dataList[4]))
 
         f.close()
-
+        print(headerList)
         print("{0}: {1}".format(headerList[1], espresso))
         print("{0}: {1}".format(headerList[2], americano))
         print("{0}: {1}".format(headerList[3], cafelatte))
         print("{0}: {1}".format(headerList[4], cappucino))
-
-        total_sum = [sum(espresso), sum(americano), sum(cafelatte), sum(cappucino)]
-        total_mean = [sum(espresso)/len(espresso), sum(americano)/len(americano),
-                      sum(cafelatte)/len(cafelatte), sum(cappucino)/len(cappucino)]
-
-        for k in range(len(total_sum)):
-            print('[{0}] 판매량'.format(headerList[k+1]))
-            print('-나흘 전체: {0}, 하루 평균: {1}'.format(total_sum[k], total_sum[k]))
-
     with open(file_name, 'r') as f:
         header = f.readline()
         print(header, header.split())
@@ -51,3 +42,33 @@ if __name__ == "__main__":
             data_list2 = dict(zip(header_list, data_list))
             print(data_list2)
 
+        total_sum = [sum(espresso), sum(americano), sum(cafelatte), sum(cappucino)]
+        total_mean = [sum(espresso) / len(espresso), sum(americano) / len(americano),
+                      sum(cafelatte) / len(cafelatte), sum(cappucino) / len(cappucino)]
+
+        for k in range(len(total_sum)):
+            print('[{0}] 판매량'.format(headerList[k + 1]))
+            print('-나흘 전체: {0}, 하루 평균: {1}'.format(total_sum[k], total_mean[k]))
+
+
+    # with open(file_name, 'r') as f:
+    #     header = f.readline()
+    #     header_list = header.split()
+    #     coffee_list =[]
+    #     [coffee_list ]
+    #
+    #     data_list.append(data_dict)
+    #
+    #     [print(data) for data in data_list]
+    #
+    #     result = {}
+    #     stat = {}
+    #     for coffee in date_list:
+    #         for k in coffee.keys():
+    #             if k != '날짜':
+    #                 result[k] = result.get(k, 0) + int(coffee[k]) # 없을때의 리턴값을 정해 줌
+    #                 stat[k] = stat.get(k, 0) + 1
+    #
+    #     print()
+    #     for p, c in zip(result.keys(), stat.keys()):
+    #         print("{} 판매량, 하루 평균{:.2f}".format(p, result[p], result[p]/stat[c]))
